30bcb9548e5abb33edd2baa5b95a1563cc6a71d7
Elton de Andrade Rodrigues <eltonrodrigues61@gmail.com>
2020-04-20 14:11:17 -0300

    fixed Union in typing

--- crawler/typing.py
+++ crawler/typing.py
@@ -2,5 +2,5 @@ from datetime import date
 from typing import Callable, Optional, Union
 
 
-FieldValue = Optional[Union[Union[date, int], str]]
-Cast = Callable[[str], Optional[Union[Union[str, date], int]]]
+FieldValue = Optional[Union[date, int, str]]
+Cast = Callable[[str], Optional[Union[str, date, int]]]
